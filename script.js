0e26b53ec352349522a0712f4e72e9cc365d8217
HeshamBarahim <heshambarahim12@gmail.com>
2022-03-04 17:32:23 +0300

    sunnyside-agency-project initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let icon = document.querySelector(".icon")
+let menu = document.querySelector(".mobile-menu")
+let element = document.querySelectorAll(".element")
+
+icon.addEventListener('click' , ()=>{
+  menu.toggle.style.display = "grid"
+})
